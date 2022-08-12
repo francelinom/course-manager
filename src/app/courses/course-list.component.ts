dcb82257b485cca071cef518f613f38c216dae5e
Francelino <fmarcilio1989@gmail.com>
2022-08-12 17:38:48 -0300

    component de course list e table de list de course

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+})
+export class CourseListComponents implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Anuglar',
+        imageUrl: '',
+        price: 99.99,
+        code: 'XPTO',
+        duration: 120,
+        rating: 5.4,
+        releaseDate: 'december, 2, 2021',
+      },
+      {
+        id: 2,
+        name: 'Anuglar 13',
+        imageUrl: '',
+        price: 199.99,
+        code: 'XYZ',
+        duration: 120,
+        rating: 5.9,
+        releaseDate: 'december, 20, 2021',
+      },
+    ];
+  }
+}
