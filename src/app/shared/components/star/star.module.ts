141f3f9f0db5f64e1aa9e178faf884670cb03fd4
Francelino <fmarcilio1989@gmail.com>
2022-08-14 11:16:13 -0300

    adcionando o module para o pipe

---
+++ src/app/shared/components/star/star.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { StarComponent } from './star.component';
+
+@NgModule({
+  declarations: [StarComponent],
+  imports: [],
+  exports: [StarComponent],
+})
+export class StarModule {}
