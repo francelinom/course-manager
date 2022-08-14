f17d3cbd92fa0a8e86faab07511d2ef053088b24
Francelino <fmarcilio1989@gmail.com>
2022-08-14 11:28:53 -0300

    adicionando o erro404 a pasta core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { Error404Component } from './404/error-404.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -23,10 +22,6 @@ import { CoreModule } from './core/core.module';
         path: '',
         redirectTo: 'courses',
         pathMatch: 'full',
-      },
-      {
-        path: '**',
-        component: Error404Component,
       },
     ]),
   ],
