82089129b459e1ead52c515b3ea1de63fc8d5df5
Francelino <fmarcilio1989@gmail.com>
2022-08-14 11:22:33 -0300

    adiconando a pasta core e adicionando o navbar a pasta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,17 @@ import { Error404Component } from './404/error-404.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '',
