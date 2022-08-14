ddd6c6655c54af939f43577656a0e39feb721fb3
Francelino <fmarcilio1989@gmail.com>
2022-08-13 22:33:25 -0300

    adicionando rotas para aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './404/error-404.component';
 
 import { AppComponent } from './app.component';
 import { CourseListComponents } from './courses/course-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 
@@ -12,9 +15,28 @@ import { StarComponent } from './star/star.component';
     AppComponent,
     CourseListComponents,
     StarComponent,
+    NavBarComponent,
     ReplacePipe,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full',
+      },
+      {
+        path: 'courses',
+        component: CourseListComponents,
+      },
+      {
+        path: '**',
+        component: Error404Component,
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+})
+export class NavBarComponent implements OnInit {
+  ngOnInit(): void {}
+}
